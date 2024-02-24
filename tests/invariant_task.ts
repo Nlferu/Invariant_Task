b77ff918e47ab493fad96a281807cbaab1b5c797
Niferu <wargacki.patryq@gmail.com>
2024-02-24 20:57:40 +0100

    test updated

--- tests/invariant_task.ts
+++ tests/invariant_task.ts
@@ -1,16 +1,17 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { InvariantTask } from "../target/types/invariant_task";
+import * as anchor from "@coral-xyz/anchor"
+import { Program } from "@coral-xyz/anchor"
+import { InvariantTask } from "../target/types/invariant_task"
 
 describe("invariant_task", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+    // Configure the client to use the local cluster.
+    anchor.setProvider(anchor.AnchorProvider.env())
 
-  const program = anchor.workspace.InvariantTask as Program<InvariantTask>;
+    const program = anchor.workspace.InvariantTask as Program<InvariantTask>
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
-});
+    it("Is initialized!", async () => {
+        const dataAccountKP = anchor.web3.Keypair.generate()
+
+        const tx = program.methods.initialize()
+        console.log("Your transaction signature: ", tx)
+    })
+})
