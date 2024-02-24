e45ee04fef2dd122bcf7e586c17dcd9c7c9beb17
Niferu <wargacki.patryq@gmail.com>
2024-02-24 20:57:34 +0100

    program update

--- programs/invariant_task/src/lib.rs
+++ programs/invariant_task/src/lib.rs
@@ -6,12 +6,57 @@ declare_id!("H3MzbSxhjW7uoW1Bqae4iNmq743KN3SjSHwcRPDdxsAq");
 pub mod invariant_task {
     use super::*;
 
-    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
-        msg!("Installation Succeeded!");
-        msg!("test message");
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let my_account = &mut ctx.accounts.my_account;
+        my_account.data = 0;
+        msg!("Program Initialized!");
+        Ok(())
+    }
+
+    pub fn update(ctx: Context<Update>, data: u64) -> Result<()> {        
+        let my_account = &mut ctx.accounts.my_account;
+        my_account.data = data;
+        Ok(())
+    }
+    
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {        
+        let my_account = &mut ctx.accounts.my_account;
+        my_account.data += 1;
+        Ok(())
+    }
+    
+    pub fn decrement(ctx: Context<Decrement>) -> Result<()> {        
+        let my_account = &mut ctx.accounts.my_account;
+        my_account.data -= 1;
         Ok(())
     }
 }
 
+#[account]
+pub struct MyAccount {
+    data: u64
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub my_account: Account<'info, MyAccount>
+}
+
+#[derive(Accounts)]
+pub struct Update<'info> {
+    #[account(mut)]
+    pub my_account: Account<'info, MyAccount>
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut)]
+    pub my_account: Account<'info, MyAccount>
+}
+
+#[derive(Accounts)]
+pub struct Decrement<'info> {
+    #[account(mut)]
+    pub my_account: Account<'info, MyAccount>
+}
