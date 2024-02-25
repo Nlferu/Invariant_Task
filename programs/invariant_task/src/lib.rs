1dd4c5270207138263ab469e9286acc26ee4f909
Niferu <wargacki.patryq@gmail.com>
2024-02-25 20:47:36 +0100

    added requirements of challenge

--- programs/invariant_task/src/lib.rs
+++ programs/invariant_task/src/lib.rs
@@ -2,6 +2,19 @@ use anchor_lang::prelude::*;
 
 declare_id!("H3MzbSxhjW7uoW1Bqae4iNmq743KN3SjSHwcRPDdxsAq");
 
+/** @TODO:
+    ● Escrow contracts should maintain separate accounts for each user, allowing efficient handling of multiple users simultaneously
+    ● Escrow contracts allow for the multi-call of entrypoints: deposit and withdraw.
+    ● Only the owner of an account should be able to withdraw funds from their respective escrow account.
+
+    ● Utilize a generic token type (SPL Token, Erc20, PSP22) based on your chosen ecosystem.
+    ● Address potential security concerns, particularly vulnerabilities like reentrancy.
+    ● Include unit tests.
+    ● Bonus points if you add end-to-end tests in TypeScript.
+    ● Bonus points if you implement deposit and withdrawal in a single transaction (swap). The swap may have different levels of complexity, e.g., always 1 to 1, AMM, order book,
+      depending on your skills.
+ */
+
 #[program]
 pub mod invariant_task {
     use super::*;
