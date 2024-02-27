a1c6e8b974a2f020a48cdc52dd733b683b7e199e
Niferu <wargacki.patryq@gmail.com>
2024-02-27 14:28:50 +0100

    tests fixed and working now

--- tests/invariant_task.ts
+++ tests/invariant_task.ts
@@ -2,120 +2,76 @@ import * as anchor from "@coral-xyz/anchor"
 import { Program } from "@coral-xyz/anchor"
 import { InvariantTask } from "../target/types/invariant_task"
 import { Keypair } from "@solana/web3.js"
-import { assert } from "chai"
+import { assert, expect } from "chai"
 
 describe("invariant_task", async () => {
     // Configure the client to use the local cluster.
-    anchor.setProvider(anchor.AnchorProvider.env())
     const provider = anchor.AnchorProvider.env()
+    anchor.setProvider(provider)
     const devil = provider.wallet
-
-    const befDevil = await provider.connection.getBalance(devil.publicKey)
-    //await provider.connection.confirmTransaction(await provider.connection.requestAirdrop(devil.publicKey, 100000), "confirmed")
-
-    const airdropSignature = await provider.connection.requestAirdrop(devil.publicKey, 2777)
-
-    const latestBlockHash = await provider.connection.getLatestBlockhash()
-
-    await provider.connection.confirmTransaction({
-        blockhash: latestBlockHash.blockhash,
-        lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
-        signature: airdropSignature,
-    })
-
-    const afBal = await provider.connection.getBalance(devil.publicKey)
-    console.log(`BEFORE BALAA: ${befDevil} AFTER ${afBal}`)
-
     const program = anchor.workspace.InvariantTask as Program<InvariantTask>
 
-    // Declaring new accounts
-    let dataAccountKP: Keypair
-    let user: Keypair
+    // Creating new account
+    const dataAccountKP = anchor.web3.Keypair.generate()
 
-    // Creating new accounts
-    dataAccountKP = anchor.web3.Keypair.generate()
-    user = anchor.web3.Keypair.generate()
+    it("Initializes contract and account correctly", async () => {
+        await program.methods
+            .initialize()
+            .accounts({ myAccount: dataAccountKP.publicKey, signer: devil.publicKey, systemProgram: anchor.web3.SystemProgram.programId })
+            .signers([dataAccountKP])
+            .rpc()
 
-    const connection = anchor.getProvider().connection
-    const befBal = await connection.getBalance(dataAccountKP.publicKey)
-    const befUserBal = await connection.getBalance(user.publicKey)
-    console.log(`Accounts Balance: ${befBal} and User: ${befUserBal}`)
-
-    await connection.requestAirdrop(dataAccountKP.publicKey, 1000)
-    await connection.requestAirdrop(user.publicKey, 1000)
-
-    const postBal = await connection.getBalance(dataAccountKP.publicKey)
-    const postUserBal = await connection.getBalance(user.publicKey)
-
-    console.log(`Accounts Balance: ${postBal} and User: ${postUserBal}`)
-    //await program.methods.initialize()
-
-    // Fetch newly created account from cluster
-    //const account = await program.account.myAccount.fetch(myAccount.publicKey)
-
-    // Check it's state was initialized
-    //assert.ok(account.data.eq(new anchor BN(0)))
-
-    //_myAccount = myAccount
-
-    console.log("Initial Acc: ", dataAccountKP.publicKey)
-
-    describe("Initialize", () => {
-        it("Initializes contract correctly", async () => {
-            const tx = program.methods
-                .initialize()
-                .accounts({ myAccount: dataAccountKP.publicKey, signer: user.publicKey, systemProgram: anchor.web3.SystemProgram.programId })
-                .signers([dataAccountKP])
-                .rpc()
-
-            assert(tx)
-        })
+        const storedAccountData = await program.account.myAccount.fetch(dataAccountKP.publicKey)
+        console.log("Account Stored Data: ", storedAccountData.data)
+        assert.equal(storedAccountData.data.toNumber(), 0)
     })
-    // describe("Update", () => {
-    //     it("Updates account data correctly", async () => {
-    //         console.log("Update Acc: ", dataAccountKP.publicKey)
-    //         const acc = await program.account.myAccount.fetch(dataAccountKP.publicKey)
-    //         assert.equal(acc.data, 0)
+    it("Updates account data correctly", async () => {
+        await program.methods.update(new anchor.BN(77)).accounts({ myAccount: dataAccountKP.publicKey }).rpc()
 
-    //         const updateTx = program.methods.update(new anchor.BN(77)).accounts({ myAccount: dataAccountKP.publicKey }).signers([dataAccountKP]).rpc()
-    //         assert(updateTx)
-    //         assert.equal(acc.data, 77)
-    //     })
-    // })
-    // describe("Increment", () => {
-    //     it("Increment account data by one", async () => {
-    //         const tx = program.methods.initialize()
-    //         assert(tx)
-
-    //         const acc = await program.account.myAccount.fetch(dataAccountKP.publicKey)
-    //         console.log(`Acc Data: ${acc.data}`)
-    //     })
-    // })
-    describe("Decrement", () => {
-        it("Decrements account data by one", async () => {
-            const tx = program.methods.initialize()
-
-            assert(tx)
-        })
+        const storedAccountData = await program.account.myAccount.fetch(dataAccountKP.publicKey)
+        console.log("Account Stored Data: ", storedAccountData.data)
+        assert.equal(storedAccountData.data.toNumber(), 77)
     })
-    describe("Execute", () => {
-        it("Creates welcome message", async () => {
-            const gmAccount = anchor.web3.Keypair.generate()
-            const user = anchor.web3.Keypair.generate()
-            const name = "Hastur"
+    it("Increments data on account by 1 correctly", async () => {
+        await program.methods.increment().accounts({ myAccount: dataAccountKP.publicKey }).rpc()
 
-            await program.methods
-                .initialize()
-                .accounts({ myAccount: gmAccount.publicKey, signer: user.publicKey, systemProgram: anchor.web3.SystemProgram.programId })
-                .signers([gmAccount, user])
-                .rpc()
+        const storedAccountData = await program.account.myAccount.fetch(dataAccountKP.publicKey)
+        console.log("Account Stored Data: ", storedAccountData.data)
+        assert.equal(storedAccountData.data.toNumber(), 78)
+    })
+    it("Decrements data on account by 1 correctly", async () => {
+        await program.methods.decrement().accounts({ myAccount: dataAccountKP.publicKey }).rpc()
 
-            // Airdrop SOL to pool account PDA
-
-            const tx = await program.methods.execute(name).accounts({ gmAccount: gmAccount.publicKey }).rpc()
-
-            const storedName = await program.account.greetingAccount.fetch(gmAccount.publicKey)
-            console.log(storedName.name)
+        const storedAccountData = await program.account.myAccount.fetch(dataAccountKP.publicKey)
+        console.log("Account Stored Data: ", storedAccountData.data)
+        assert.equal(storedAccountData.data.toNumber(), 77)
+    })
+    it("Executes welcome message correctly", async () => {
+        // Example funding account
+        const prevDevilBalance = await provider.connection.getBalance(devil.publicKey)
+        const airdropSignature = await provider.connection.requestAirdrop(devil.publicKey, 2750)
+        const latestBlockHash = await provider.connection.getLatestBlockhash()
+        await provider.connection.confirmTransaction({
+            blockhash: latestBlockHash.blockhash,
+            lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+            signature: airdropSignature,
         })
+
+        const postDevilBalance = await provider.connection.getBalance(devil.publicKey)
+        console.log(`Starting Devil Balance: ${prevDevilBalance} Post Devil Balance: ${postDevilBalance}`)
+
+        const gmAccount = anchor.web3.Keypair.generate()
+        const name = "Anu"
+
+        await program.methods
+            .execute(name)
+            .accounts({ gmAccount: gmAccount.publicKey, user: devil.publicKey, systemProgram: anchor.web3.SystemProgram.programId })
+            .signers([gmAccount])
+            .rpc()
+
+        const storedName = await program.account.greetingAccount.fetch(gmAccount.publicKey)
+        console.log(storedName.name)
+
+        assert.equal(storedName.name, name)
     })
 })
