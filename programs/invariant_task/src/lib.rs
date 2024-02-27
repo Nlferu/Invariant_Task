cdabb0071870bce1effd3aaceb9f027f90a58098
Niferu <wargacki.patryq@gmail.com>
2024-02-27 20:52:50 +0100

    building escrow

--- programs/invariant_task/src/lib.rs
+++ programs/invariant_task/src/lib.rs
@@ -19,82 +19,23 @@ declare_id!("FH5mcTB7dWgiNjs2JFNRXzyMk587oX7RrZTWtVbPPmFC");
 pub mod invariant_task {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        let my_account = &mut ctx.accounts.my_account;
-        my_account.data = 0;
-        msg!("Program Initialized!");
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        _vault_account_bump: u8,
+        initializer_amount: u64,
+        taker_amount: u64,
+    ) -> ProgramResult {
+        // TODO
         Ok(())
     }
 
-    pub fn execute(ctx: Context<Execute>, name: String) -> Result<()> {
-        let gm_account = &mut ctx.accounts.gm_account;
-        gm_account.name = name;
-        msg!("Hello, {}", gm_account.name);
+    pub fn cancel(ctx: Context<Cancel>) -> ProgramResult {
+        // TODO
         Ok(())
     }
 
-    pub fn update(ctx: Context<Update>, data: u64) -> Result<()> {        
-        let my_account = &mut ctx.accounts.my_account;
-        my_account.data = data;
-        Ok(())
-    }
-    
-    pub fn increment(ctx: Context<Increment>) -> Result<()> {        
-        let my_account = &mut ctx.accounts.my_account;
-        my_account.data += 1;
-        Ok(())
-    }
-    
-    pub fn decrement(ctx: Context<Decrement>) -> Result<()> {        
-        let my_account = &mut ctx.accounts.my_account;
-        my_account.data -= 1;
+    pub fn exchange(ctx: Context<Exchange>) -> ProgramResult {
+        // TODO
         Ok(())
     }
 }
-
-#[account]
-pub struct MyAccount {
-    pub data: u64
-}
-
-#[account]
-pub struct GreetingAccount {
-    pub name: String,
-}
-
-#[derive(Accounts)]
-pub struct Initialize<'info> {
-    #[account(init, payer = signer, space = 8 + 8)]
-    pub my_account: Account<'info, MyAccount>,
-    #[account(mut)]
-    pub signer: Signer<'info>,
-    pub system_program: Program<'info, System>,
-}
-
-#[derive(Accounts)]
-pub struct Execute<'info> {
-    #[account(init, payer = user, space = 8 + 32)]
-    pub gm_account: Account<'info, GreetingAccount>,
-    #[account(mut)]
-    pub user: Signer<'info>,
-    pub system_program: Program<'info, System>,
-}
-
-#[derive(Accounts)]
-pub struct Update<'info> {
-    #[account(mut)]
-    pub my_account: Account<'info, MyAccount>
-}
-
-#[derive(Accounts)]
-pub struct Increment<'info> {
-    #[account(mut)]
-    pub my_account: Account<'info, MyAccount>
-}
-
-#[derive(Accounts)]
-pub struct Decrement<'info> {
-    #[account(mut)]
-    pub my_account: Account<'info, MyAccount>
-}
-
