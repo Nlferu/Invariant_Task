2712640fbc6fe8daccf9edcab9634a6cd503c7ee
Niferu <wargacki.patryq@gmail.com>
2024-02-28 03:17:04 +0100

    building escrow

--- programs/invariant_task/src/lib.rs
+++ programs/invariant_task/src/lib.rs
@@ -1,41 +1,104 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token;
+use anchor_spl::token::{MintTo, Token, Transfer};
 
-declare_id!("FH5mcTB7dWgiNjs2JFNRXzyMk587oX7RrZTWtVbPPmFC");
+declare_id!("EMjE3UTNgvN79M5utukj1Q6UJqpFYinF7AxCV1oAY3gZ");
 
 /** @TODO:
-    ● Escrow contracts should maintain separate accounts for each user, allowing efficient handling of multiple users simultaneously
-    ● Escrow contracts allow for the multi-call of entrypoints: deposit and withdraw.
-    ● Only the owner of an account should be able to withdraw funds from their respective escrow account.
+   ● Escrow contracts should maintain separate accounts for each user, allowing efficient handling of multiple users simultaneously
+   ● Escrow contracts allow for the multi-call of entrypoints: deposit and withdraw.
+   ● Only the owner of an account should be able to withdraw funds from their respective escrow account.
 
-    ● Utilize a generic token type (SPL Token, Erc20, PSP22) based on your chosen ecosystem.
-    ● Address potential security concerns, particularly vulnerabilities like reentrancy.
-    ● Include unit tests.
-    ● Bonus points if you add end-to-end tests in TypeScript.
-    ● Bonus points if you implement deposit and withdrawal in a single transaction (swap). The swap may have different levels of complexity, e.g., always 1 to 1, AMM, order book,
-      depending on your skills.
- */
+   ● Utilize a generic token type (SPL Token, Erc20, PSP22) based on your chosen ecosystem.
+   ● Address potential security concerns, particularly vulnerabilities like reentrancy.
+   ● Include unit tests.
+   ● Bonus points if you add end-to-end tests in TypeScript.
+   ● Bonus points if you implement deposit and withdrawal in a single transaction (swap). The swap may have different levels of complexity, e.g., always 1 to 1, AMM, order book,
+     depending on your skills.
+*/
 
 #[program]
 pub mod invariant_task {
     use super::*;
 
-    pub fn initialize(
-        ctx: Context<Initialize>,
-        _vault_account_bump: u8,
-        initializer_amount: u64,
-        taker_amount: u64,
-    ) -> ProgramResult {
-        // TODO
+    pub fn mint_token_a(ctx: Context<MintToken>) -> Result<()> {
+        // Create the MintTo struct for our context
+        let cpi_accounts = MintTo {
+            mint: ctx.accounts.mint.to_account_info(),
+            to: ctx.accounts.token_account.to_account_info(),
+            authority: ctx.accounts.authority.to_account_info(),
+        };
+
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        // Create the CpiContext we need for the request
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+
+        // Execute anchor's helper function to mint tokens
+        token::mint_to(cpi_ctx, 10)?;
+
         Ok(())
     }
 
-    pub fn cancel(ctx: Context<Cancel>) -> ProgramResult {
-        // TODO
+    pub fn mint_token_b(ctx: Context<MintToken>) -> Result<()> {
+        // Create the MintTo struct for our context
+        let cpi_accounts = MintTo {
+            mint: ctx.accounts.mint.to_account_info(),
+            to: ctx.accounts.token_account.to_account_info(),
+            authority: ctx.accounts.authority.to_account_info(),
+        };
+
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        // Create the CpiContext we need for the request
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+
+        // Execute anchor's helper function to mint tokens
+        token::mint_to(cpi_ctx, 10)?;
+
         Ok(())
     }
 
-    pub fn exchange(ctx: Context<Exchange>) -> ProgramResult {
-        // TODO
+    pub fn transfer_token(ctx: Context<TransferToken>) -> Result<()> {
+        // Create the Transfer struct for our context
+        let transfer_instruction = Transfer {
+            from: ctx.accounts.from.to_account_info(),
+            to: ctx.accounts.to.to_account_info(),
+            authority: ctx.accounts.from_authority.to_account_info(),
+        };
+
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        // Create the Context for our Transfer request
+        let cpi_ctx = CpiContext::new(cpi_program, transfer_instruction);
+
+        // Execute anchor's helper function to transfer tokens
+        anchor_spl::token::transfer(cpi_ctx, 5)?;
+
         Ok(())
     }
+}
+
+#[derive(Accounts)]
+pub struct MintToken<'info> {
+    /// CHECK: This is the token that we want to mint
+    #[account(mut)]
+    pub mint: UncheckedAccount<'info>,
+    pub token_program: Program<'info, Token>,
+    /// CHECK: This is the token account that we want to mint tokens to
+    #[account(mut)]
+    pub token_account: UncheckedAccount<'info>,
+    /// CHECK: the authority of the mint account can allow us to mint tokens to our account
+    #[account(mut)]
+    pub authority: AccountInfo<'info>,
+}
+
+#[derive(Accounts)]
+pub struct TransferToken<'info> {
+    pub token_program: Program<'info, Token>,
+    /// CHECK: The associated token account that we are transferring the token from
+    #[account(mut)]
+    pub from: UncheckedAccount<'info>,
+    /// CHECK: The associated token account that we are transferring the token to
+    #[account(mut)]
+    pub to: AccountInfo<'info>,
+    // the authority of the from account can allow us to mint tokens to our account
+    pub from_authority: Signer<'info>,
 }
